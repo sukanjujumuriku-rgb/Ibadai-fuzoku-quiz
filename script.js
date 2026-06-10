add1e2cc67e882faf0dc33d012447ef09793a245
sukanjujumuriku-rgb <sukanjujumuriku@gmail.com>
2026-06-10 09:23:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -285,3 +285,13 @@ function showResult() {
         </button>
     `;
 }
+function shuffle(array) {
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
